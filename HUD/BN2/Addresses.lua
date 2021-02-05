5cdbd3f8bfdc3bf51bc31e15447bb74eb97126b8
Jarrett <2821955+JarrettB42@users.noreply.github.com>
2021-02-05 06:12:53 -0500

    BN2: Added some RAM and Progress values

--- HUD/BN2/Addresses.lua
+++ HUD/BN2/Addresses.lua
@@ -44,63 +44,97 @@ addresses.library             = 0x02000060; -- ? bytes, bit flags, ends at 82?
 -- 10A Pause state?
 -- 10B Don't write to this byte...
 
---addresses.                  = 0x02000DC0; -- 1 byte, start of important things?
+addresses.folder_1_ID         = 0x02000AB0; -- every other 2 bytes, chip  IDs  of folder 1, ends at B24
+addresses.folder_1_code       = 0x02000AB2; -- every other 2 bytes, chip codes of folder 1, ends at B26
+
+-- DC0 start of important things?
+
+addresses.game_state_also     = 0x02000DC0; -- 1 byte?
 addresses.main_area           = 0x02000DC4; -- 1 byte
 addresses.sub_area            = 0x02000DC5; -- 1 byte
-addresses.progress            = 0x02000000; -- 1 byte
-addresses.music_progress      = 0x02000000; -- 1 byte
+addresses.battle_paused       = 0x02000DC9; -- 1 byte, flag 0x01
+addresses.progress            = 0x02000DCA; -- 1 byte
+addresses.music_progress      = 0x02000DCB; -- 1 byte
+addresses.is_talking          = 0x02000DDA; -- 1 byte, with NPC/MD, flag 0x01
+addresses.is_interacting      = 0x02000DDA; -- 1 byte, toggle check, flag 0x01
 
---addresses.                  = 0x02009070; -- 4 bytes, state transition related?
-addresses.game_state          = 0x02009078; -- 1 byte
-addresses.RNG                 = 0x02009080; -- 4 bytes, restarts on the title screen
-addresses.battle_state        = 0x02000000; -- 1 byte
-addresses.folder_menu_state   = 0x02000000; -- 1 byte
+addresses.zenny               = 0x02000E34; -- 4 bytes, 999999 "max"
+
+-- E3B end of important things?
 
 addresses.style_active        = 0x02000000; -- 1 byte
 addresses.style_stored_1      = 0x02000000; -- 1 byte
 addresses.style_stored_2      = 0x02000000; -- 1 byte
 
-addresses.zenny               = 0x02000000; -- 4 bytes, 999999 "max"
 addresses.element_next        = 0x0200112B; -- 1 byte, element of next style?
 addresses.steps               = 0x02001150; -- 4 bytes
-addresses.steps_menu          = 0x02008828; -- 4 bytes? steps since last menu?
 addresses.steps_total         = 0x0200113C; -- 4 bytes? steps since new game?
+addresses.play_time_frames    = 0x02001144; -- 4 bytes, check for skipped frames
 addresses.check               = 0x02001154; -- 4 bytes, steps at the last encounter check
 addresses.sneak               = 0x02001178; -- 4? bytes, starts at 6000
 
+addresses.custom_gauge_battle = 0x02004F0C; -- 2 bytes, counts up to 0x4000
+
+addresses.points_guts         = 0x02003A5D; -- 2 bytes
+addresses.points_cust         = 0x02003A62; -- 2 bytes
+addresses.points_team         = 0x02003A64; -- 2 bytes
+addresses.points_shld         = 0x02003A66; -- 2 bytes
+-- ties go in order of: shield, custom, team, guts? (according to faq)
+addresses.battles_guts        = 0x02003A68; -- 4 bytes
+addresses.battles_cust        = 0x02003A6C; -- 4 bytes
+addresses.battles_team        = 0x02003A70; -- 4 bytes
+addresses.battles_shld        = 0x02003A74; -- 4 bytes
+
+addresses.enemy_ID            = 0x02004F3C; -- 1 byte
+addresses.enemy_ID_2          = 0x02004F3D; -- 1 byte
+addresses.enemy_ID_3          = 0x02004F3F; -- 1 byte
+
 addresses.battle_draw_slots   = 0x02005EC0; -- 1 byte each, in battle chip draws, ends at 492D
+
+addresses.chip_cooldown       = 0x0200626B; -- 1 byte ???
 
 addresses.your_X              = 0x02006374; -- 2 bytes
 addresses.your_Y              = 0x02006376; -- 2 bytes
 addresses.your_X2             = 0x02000000; -- 2 bytes
 addresses.your_Y2             = 0x02000000; -- 2 bytes
 
-addresses.folder_1_ID         = 0x02000AB0; -- every other 2 bytes, chip  ID  of folder slot 1, ends at ???
---2 bytes, chip ID of folder 1 slot 1
--- folder_2 2000AB4
--- folder_2 2000AB6
-addresses.folder_1_code       = 0x02000AB2; -- every other 2 bytes, chip code of folder slot 1, ends at ???
---2 bytes, chip Code of folder 1 slot 1
+--addresses.                  = 0x0200EEF0; -- 1 byte, battle data
+--addresses.                  = 0x0200EEF0; -- 1 byte, battle data
+--addresses.                  = 0x0200EEF1; -- 1 byte, custom menu state: 0x04 chip select 0x0C OK 0x10 ADD
+--addresses.                  = 0x0200EEF4; -- 1 byte, battle data
+addresses.custom_hover_slot   = 0x0200EEF5; -- 1 byte
+addresses.custom_hover_slot   = 0x0200EF38; -- 1 byte each, select visual state: 0x00 normal 0x01 selected 0x02 black out
+addresses.custom_folder_slot  = 0x0200EF42; -- 1 byte each, the selected folder slots (in order)
+addresses.custom_select_slot  = 0x0200EF47; -- 1 byte each, the selected  chip  slots (in order)
+addresses.custom_select_code  = 0x0200EF51; -- 1 byte each, the selected  chip  codes (in order)
+addresses.custom_gauge_fill   = 0x0200EF91; -- 1 byte, counts up to 0x80
+addresses.custom_gauge_visual = 0x0200EF91; -- 1 byte, loops
+addresses.battle_visual_state = 0x0200EFA5; -- 1 byte, normal, BATTLE START!, PAUSE
+addresses.battle_timer        = 0x0200EFBA; -- 2 bytes, frame counter for current battle (doesn't pause?)
+
+addresses.cursor_ID           = 0x02007EA8; -- 1 byte, chip  ID  of cursor
+addresses.cursor_code         = 0x02007EAA; -- 1 byte, chip code of cursor
+addresses.folder_count        = 0x02007EA5; -- 1 byte, number of chips in folder
+addresses.cursor_pack         = 0x02007EBC; -- 2 bytes, cursor value in the pack
+addresses.offset_pack         = 0x02007EC0; -- 2 bytes, offset value in the pack
+addresses.offset_selected_pack   = 0x02007EF0; -- 2 bytes, offset value of selected chip
+addresses.cursor_selected_pack   = 0x02007EF2; -- 2 bytes, cursor value of selected chip
+addresses.offset_selected_folder = 0x02007EE8; -- 2 bytes, offset value of selected chip
+addresses.cursor_selected_folder = 0x02007EEA; -- 2 bytes, cursor value of selected chip
+
+addresses.steps_menu          = 0x02008828; -- 4 bytes? steps since last menu?
+addresses.power_on_frames     = 0x02008880; -- 2 bytes, session counter
+
+--addresses.                  = 0x02009070; -- 4 bytes, state transition related?
+addresses.game_state          = 0x02009078; -- 1 byte
+addresses.RNG                 = 0x02009080; -- 4 bytes, restarts on the title screen
+
+addresses.folder_menu_state   = 0x02000000; -- 1 byte
 
 -- TODO: V
 
-addresses.paused = 0x02000DC9; -- ? bytes
--- ties go in order of: shield, custom, team, guts? (according to faq)
-addresses.battles_guts = 0x02003A68; -- 4 bytes
-addresses.battles_cust = 0x02003A6C; -- 4 bytes
-addresses.battles_team = 0x02003A70; -- 4 bytes
-addresses.battles_shld = 0x02003A74; -- 4 bytes
-
-addresses.points_guts = 0x02003A5D; -- 2 bytes
-addresses.points_cust = 0x02003A62; -- 2 bytes
-addresses.points_team = 0x02003A64; -- 2 bytes
-addresses.points_shld = 0x02003A66; -- 2 bytes
-
 addresses.BMD                 = 0x02000000; -- bit flags
 addresses.GMD_reward          = 0x02000000; -- 2 bytes, how to decode?
-addresses.steps_total         = 0x02000000; -- 3 bytes, since new game
-addresses.play_time_frames    = 0x02001144; -- 4 bytes, check for skipped frames
-addresses.power_on_frames     = 0x02000000; -- 2 bytes, session counter
 addresses.button_flags        = 0x02000000; -- many bytes, many flags
 
 addresses.HPMemory            = 0x02000000; -- 1 byte, collected
@@ -110,29 +144,14 @@ addresses.buster_rapid        = 0x02000000; -- 1 byte, 0 indexed, can't change m
 addresses.buster_charge       = 0x02000000; -- 1 byte, 0 indexed, can't change mid-battle
 
 addresses.battle_pointer      = 0x02000000; -- 2 bytes? ROM offset?
-addresses.battle_custom_gauge = 0x02000000; -- 2 bytes, counts up to 0x4000
-addresses.chip_cooldown       = 0x02000000; -- 1 byte, BstrBomb HYPE
-addresses.enemy_ID            = 0x02000000; -- 1 byte
-addresses.enemy_ID_2          = 0x02000000; -- 1 byte
-addresses.enemy_ID_3          = 0x02000000; -- 1 byte
 addresses.enemy_HP            = 0x02000000; -- 2 bytes, which_enemy * 0xC0
 addresses.enemy_HP_2          = 0x02000000; -- 2 bytes, which_enemy * 0xC0
 addresses.enemy_HP_3          = 0x02000000; -- 2 bytes, which_enemy * 0xC0
 
-addresses.cursor_ID           = 0x02007EA8; -- 1 byte, chip  ID  of cursor
-addresses.cursor_code         = 0x02007EAA; -- 1 byte, chip code of cursor
-
 addresses.pack_ID             = 0x02000000; -- 1 byte, chip  ID  of pack slot 1
 addresses.pack_code           = 0x02000000; -- 1 byte, chip code of pack slot 1
-addresses.folder_count        = 0x02007EA5; -- 1 byte, number of chips in folder
 addresses.cursor_folder       = 0x02000000; -- 2 bytes, cursor value in the folder
 addresses.offset_folder       = 0x02000000; -- 2 bytes, offset value in the folder
-addresses.cursor_pack         = 0x02007EBC; -- 2 bytes, cursor value in the pack
-addresses.offset_pack         = 0x02007EC0; -- 2 bytes, offset value in the pack
-addresses.offset_selected_pack = 0x02007EF0; -- 2 bytes, offset value of selected chip
-addresses.cursor_selected_pack = 0x02007EF2; -- 2 bytes, cursor value of selected chip
-addresses.offset_selected_folder = 0x02007EE8; -- 2 bytes, offset value of selected chip
-addresses.cursor_selected_folder = 0x02007EEA; -- 2 bytes, cursor value of selected chip
 addresses.cursor_sort         = 0x02000000; -- 2 bytes, sort menu cursor position
 
 -- 0x0203FFFF end of WRAM?
--- HUD/BN2/Progress.lua
+++ HUD/BN2/Progress.lua
@@ -5,8 +5,16 @@
 local progress = {};
 
 progress[0x00] = "New Game";
-progress[0x01] = "Tutorial Start";
-progress[0x02] = "Tutorial Finished";
+progress[0x01] = "Talked to Dex";
+progress[0x02] = "Tutorial Done";
+progress[0x03] = "ZLicense Exam";
+progress[0x04] = "ZLicense Pass";
+progress[0x05] = "Talked to Dex";
+progress[0x06] = "Ventilator Cleared";
+progress[0x07] = "AirMan Deleted";
+progress[0x08] = "Gone to bed";
+
+progress[0x10] = "";
 
 progress[0xFE] = "Final State";
 
