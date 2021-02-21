fca3d1fad2d7ff26cad15ec47184b092cf215446
Jarrett <2821955+JarrettB42@users.noreply.github.com>
2021-02-21 11:15:02 -0500

    BN4: Added link to enemy IDs

--- HUD/BN4/Enemies.lua
+++ HUD/BN4/Enemies.lua
@@ -1,11 +1,14 @@
 -- Enemy names for MMBN 4 scripting, enjoy.
 
--- TODO: Add enemy IDs for BN4
+-- https://forums.therockmanexezone.com/mmbn4-custom-battle-guide-t5349.html TODO
 
 local enemies = {};
 
 enemies.names = {};
 
+enemies.names[0x0000] = "Empty";
+enemies.names[0x00FF] = "Empty";
+enemies.names[0x01FF] = "Empty";
 enemies.names[0xFFFF] = "Empty";
 
 return enemies;
