ec19a591296850528218a2a3d66944cdfa954f7a
Jarrett <2821955+JarrettB42@users.noreply.github.com>
2021-09-01 15:43:56 -0400

    Sim: BN3 Folder Shuffling fully functional

--- Simulations/rng.py
+++ Simulations/rng.py
@@ -27,51 +27,80 @@ def iterate_RNG(seed, iterations):
     return seed
 #def
 
-def calculate_RNG_delta(temp, goal):
+max_delta = 100000
+def calculate_RNG_delta(seed, goal):
     delta = 0
-    while temp != goal and delta < 100000:
+    while seed != goal:
         delta += 1
-        temp = simulate_RNG(temp)
+        seed = simulate_RNG(seed)
+        if delta > max_delta:
+            return None
+        #if
     #while
     return delta
 #def
 
-def shuffle_folder_bn3(RNG_value=0x873CA9E4): # incorrect output
+def shuffle_folder_bn3(RNG_value=0x873CA9E4, reg_slot=255, swaps=30): # matches L Delta of 61
     slots = 30
-    swaps = 30
     
     draw_slots = [n for n in range(slots)]
     
-    #print(draw_slots)
+    if reg_slot != 255:
+        slots -= 1
+        reg_slot -= 1 # 0 index
+        draw_slots.pop(reg_slot)
+    #if
     
     for i in range(swaps):
+        slot_a = to_int(RNG_value) % slots
         RNG_value = simulate_RNG(RNG_value)
-        slot_a = (RNG_value & 0x7FFFFFFF) % slots
+        slot_b = to_int(RNG_value) % slots
         RNG_value = simulate_RNG(RNG_value)
-        slot_b = (RNG_value & 0x7FFFFFFF) % slots
-        #print(f'swapping {slot_a:02d} and {slot_b:02d}')
         draw_slots[slot_a], draw_slots[slot_b] = draw_slots[slot_b], draw_slots[slot_a]
     #for
     
-    #print(draw_slots)
+    if reg_slot != 255:
+        draw_slots.insert(reg_slot, 0)
+    #if
     
     return draw_slots
 #def
 
-def print_draw_slots_bn3(target_index, reg_slot=255):
-    draw_slots = shuffle_folder_bn3(iterate_RNG(0x873CA9E4, target_index-60))
-    draw_string = str(target_index) + ":"
-    for draw_slot in draw_slots:
-        draw_string += f' {draw_slot+1:02d}'
+def get_draw_slots_bn3(start_index, end_index, reg_slot=255):
+    draw_slots = {}
+    RNG_value = iterate_RNG(0x873CA9E4, start_index-61)
+    for index in range(start_index, end_index+1):
+        draw_slots[index] = shuffle_folder_bn3(RNG_value, reg_slot)
+        RNG_value = simulate_RNG(RNG_value)
     #for
-    print(draw_string)
+    return draw_slots
 #def
 
-test_index = 120
-print_draw_slots_bn3(test_index+0)
-print_draw_slots_bn3(test_index+2)
-print_draw_slots_bn3(test_index+4)
-print_draw_slots_bn3(test_index+6)
+def print_draw_slots_bn3(start_index, end_index, reg_slot=255):
+    draw_frames = get_draw_slots_bn3(start_index, end_index, reg_slot)
+    for draw_frame in draw_frames:
+        draw_string = f'{draw_frame:d}'
+        for draw_slot in draw_frames[draw_frame]:
+            draw_string += f' {draw_slot+1:d}'
+        #for
+        print(draw_string)
+    #for
+#def
+
+def print_draw_slots_log_bn3(start_index, end_index, reg_slot=255):
+    print("####: 01 02 03 04 05 | 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30")
+    print("---------------------+---------------------------------------------------------------------------")
+    draw_frames = get_draw_slots_bn3(start_index, end_index, reg_slot)
+    for draw_frame in draw_frames:
+        draw_string = f'{draw_frame:04d}:'
+        for draw_slot in draw_frames[draw_frame]:
+            draw_string += f' {draw_slot+1:02d}'
+        #for
+        print(draw_string[:21] + "|" + draw_string[20:])
+    #for
+#def
+
+#print_draw_slots_log_bn3(-10, 120, 1)
 
 def shuffle_folder_simulate_from_value(RNG_value, swaps, reg_slot):
     RNG_value = RNG_value or 0x873CA9E5
