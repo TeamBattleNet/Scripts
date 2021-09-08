fc74f6e6a92dce56b028730ac4812ddba8946e29
Jarrett <2821955+JarrettB42@users.noreply.github.com>
2021-09-08 10:21:39 -0400

    BN3: Added GMD locations to routing HUD

--- HUD/BN3/HUD.lua
+++ HUD/BN3/HUD.lua
@@ -66,14 +66,18 @@ local function HUD_routing()
     hud.set_offset( 0, hud.y+1);
     --]]
     hud.to_screen("GMD RNG: " .. hud.game.get_string_binary(0x02000E68,  4, true));
-    hud.set_offset( 0, hud.y+1);
+    hud.set_offset(34, hud.y+1);
     hud.display_both_RNG(false, true);
-    hud.set_offset(27, hud.y-6);
+    hud.set_offset( 0, hud.y-6);
     hud.to_screen(string.format("GMD RNG  : 0x%08X", hud.game.get_GMD_RNG()));
     hud.to_screen(string.format("RNG Index: %10s",  (hud.game.get_GMD_RNG_index() or "????")));
     hud.to_screen(string.format("GMD Value: 0x%08X", hud.game.get_GMD_value()));
     hud.to_screen(string.format("GMD Virus: %s", tostring(hud.game.get_GMD_is_virus())));
     hud.to_screen(string.format("GMD Index: %2u",    hud.game.get_GMD_index()));
+    hud.to_screen(string.format("GMD 1  XY: 0x%04X 0x%04X",
+        hud.game.ram.get.GMD_1_xy(), hud.game.ram.get.GMD_1_yx()));
+    hud.to_screen(string.format("GMD 2  XY: 0x%04X 0x%04X",
+        hud.game.ram.get.GMD_2_xy(), hud.game.ram.get.GMD_2_yx()));
     hud.reset_xy();
     hud.set_position(2, 17);
     hud.display_steps(true);
