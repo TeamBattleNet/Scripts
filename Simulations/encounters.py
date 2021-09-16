42d3024378c3914635bd6eca9810b0feb3d0de48
Jarrett <2821955+JarrettB42@users.noreply.github.com>
2021-09-16 11:27:05 -0400

    Sim: Added helper function to Encounter Window Tool

--- Simulations/encounters.py
+++ Simulations/encounters.py
@@ -2,15 +2,13 @@
 
 import rng
 
-max_wiggles = 0
-A_offset    = 0
-index_start = 1
-index_end   = 1
-constraints = [{
-    "offset"       :     0,
-    "threshold"    :  0x00,
-    "got_encounter": False,
-}]
+max_wiggles  = 0
+A_offset     = 0
+index_start  = 1
+index_end    = 1
+area_curve   = 0
+check_number = 1
+constraints  = []
 
 def check_frame(mRNG):
     for constraint in constraints:
@@ -33,6 +31,28 @@ def find_windows():
     #for
 #def
 
+encounter_thresholds = [                              # BN 3
+    [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], #    0
+    [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], #   64
+    [0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], #  128
+    [0x02, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00], #  192
+    [0x03, 0x02, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00], #  256
+    [0x04, 0x03, 0x02, 0x01, 0x00, 0x00, 0x00, 0x00], #  320
+    [0x05, 0x04, 0x03, 0x02, 0x01, 0x00, 0x00, 0x00], #  384
+    [0x06, 0x05, 0x04, 0x03, 0x02, 0x00, 0x00, 0x00], #  448
+    [0x08, 0x06, 0x05, 0x04, 0x03, 0x01, 0x00, 0x00], #  512
+    [0x0A, 0x08, 0x06, 0x05, 0x04, 0x02, 0x00, 0x00], #  576
+    [0x0C, 0x0A, 0x08, 0x06, 0x05, 0x03, 0x00, 0x00], #  640
+    [0x0E, 0x0C, 0x0A, 0x08, 0x06, 0x04, 0x00, 0x00], #  704
+    [0x10, 0x0E, 0x0C, 0x0A, 0x08, 0x05, 0x00, 0x00], #  768
+    [0x12, 0x10, 0x0E, 0x0C, 0x0A, 0x06, 0x00, 0x00], #  832
+    [0x14, 0x12, 0x10, 0x0E, 0x0C, 0x08, 0x02, 0x00], #  896
+    [0x1A, 0x14, 0x12, 0x10, 0x0E, 0x0A, 0x06, 0x00], #  960
+    [0x1C, 0x1A, 0x14, 0x12, 0x10, 0x0C, 0x0C, 0x00], # 1024
+]
+#encounter_thresholds[check_number][area_curve]
+# BugRun uses Area Curve 0
+
 """ Example Encounter Curves (BN 3):
 #: 00 01 02 03 04 05 06 07 - step
 ---------------------------------
@@ -53,7 +73,7 @@ D: 12 10 0E 0C 0A 06 00 00 -  832
 E: 14 12 10 0E 0C 08 02 00 -  896
 F: 1A 14 12 10 0E 0A 06 00 -  960
 G: 1C 1A 14 12 10 0C 0C 00 - 1024
-BugRun uses Curve 00
+BugRun uses Area Curve 00
 
 -- ram.would_get_encounter(0x439E54F2); -- true
 -- 0x439E54F2 01000011100111100101010011110010 input
@@ -70,6 +90,16 @@ BugRun uses Curve 00
 -- 0xFFFFFFFF 11111111111111111111111111111111 no encounter
 """
 
+def add_constraint(offset, got_encounter):
+    global check_number
+    constraints.append({
+        "offset"       : offset,
+        "threshold"    : encounter_thresholds[min(check_number-1,15)][(area_curve-1)%8],
+        "got_encounter": got_encounter,
+    })
+    check_number = check_number + 1
+#def
+
 #print(f"0x{rng.reverse_RNG(0x02-1):08x}")
 #print(f"0x{rng.reverse_RNG(0x06-1):08x}")
 #print(f"0x{rng.reverse_RNG(0x0C-1):08x}")
@@ -77,15 +107,14 @@ BugRun uses Curve 00
 print("")
 
 # Single Threshold Check
-max_wiggles = 0
-A_offset    = 0
-index_start =    1
-index_end   = 2000
-constraints = [{
-    "offset"       :    0,
-    "threshold"    : 0x0C,
-    "got_encounter": True,
-}]
+max_wiggles  = 0
+A_offset     = 0
+index_start  =    1
+index_end    = 2000
+constraints  = []
+area_curve   = 5
+check_number = 16
+add_constraint(0, True)
 #find_windows()
 
 # IceBall M[anip] Go Fast (lRNG 1267)
@@ -145,45 +174,18 @@ constraints = [
 # YoYo Yort Beach 1
 # Area Curve 6: 1st, 2nd, 3rd, 5th, 6th, and 7th (so not 4th or 8th)
 max_wiggles = 0
-A_offset    = 64
+A_offset    = 64  # 56 steps to next check
 index_start = 124
 index_end   = 999
-constraints = [
-    {
-        "offset"       :     0,
-        "threshold"    :  0x02,
-        "got_encounter":  True,
-    },
-    {
-        "offset"       :  33*1,
-        "threshold"    :  0x06,
-        "got_encounter":  True,
-    },
-    {
-        "offset"       :  33*2,
-        "threshold"    :  0x0C,
-        "got_encounter":  True,
-    },
-    {
-        "offset"       :  33*3,
-        "threshold"    :  0x0C,
-        "got_encounter": False,
-    },
-    {
-        "offset"       :  33*4,
-        "threshold"    :  0x0C,
-        "got_encounter":  True,
-    },
-    {
-        "offset"       :  33*5,
-        "threshold"    :  0x0C,
-        "got_encounter":  True,
-    },
-    {
-        "offset"       :  33*6,
-        "threshold"    :  0x0C,
-        "got_encounter":  True,
-    },
-]
+area_curve   = 6
+check_number = 14
+constraints = []
+add_constraint(33*0,  True)
+add_constraint(33*1,  True)
+add_constraint(33*2,  True)
+add_constraint(33*3, False)
+add_constraint(33*4,  True)
+add_constraint(33*5,  True)
+add_constraint(33*6,  True)
 #find_windows()
 
